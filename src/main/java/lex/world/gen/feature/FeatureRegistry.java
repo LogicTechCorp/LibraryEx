73d46685b8272968c229655f3acb200f86b741d6
Unknown <logictechcorp@gmail.com>
2018-02-10 17:28:55 -0700

    Updated copyright years

--- src/main/java/lex/world/gen/feature/FeatureRegistry.java
+++ src/main/java/lex/world/gen/feature/FeatureRegistry.java
@@ -1,6 +1,6 @@
 /*
  * LibEx
- * Copyright (c) 2017 by MineEx
+ * Copyright (c) 2017-2018 by MineEx
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
