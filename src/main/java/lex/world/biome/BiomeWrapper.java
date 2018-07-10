d3f7e7115fecb983b74cadb2f1f1054a04e0d068
LogicTechCorp <logictechcorp@gmail.com>
2018-07-09 19:57:54 -0700

    Fixed crashing if wrapped biome was null

--- src/main/java/lex/world/biome/BiomeWrapper.java
+++ src/main/java/lex/world/biome/BiomeWrapper.java
@@ -56,100 +56,104 @@ public class BiomeWrapper
     private void parse()
     {
         biome = ForgeRegistries.BIOMES.getValue(config.getResource("biome"));
-        weight = config.getInt("weight", 10);
-        Config blockConfig = config.getDataBranch("blocks", new JsonObject());
-        blockConfig.getBlock("topBlock", biome.topBlock);
-        blockConfig.getBlock("fillerBlock", biome.fillerBlock);
 
-        for(Map.Entry<String, JsonElement> entry : blockConfig.getAllData().entrySet())
+        if(biome != null)
         {
-            if(blockConfig.getBlock(entry.getKey()) != null)
+            weight = config.getInt("weight", 10);
+            Config blockConfig = config.getDataBranch("blocks", new JsonObject());
+            blockConfig.getBlock("topBlock", biome.topBlock);
+            blockConfig.getBlock("fillerBlock", biome.fillerBlock);
+
+            for(Map.Entry<String, JsonElement> entry : blockConfig.getAllData().entrySet())
             {
-                blocks.put(entry.getKey(), blockConfig.getBlock(entry.getKey()));
-            }
-        }
-
-        List<Config> entityConfigs = config.getDataBranches("entities", new ArrayList<>());
-        List<JsonObject> entityObjects = new ArrayList<>();
-
-        for(EnumCreatureType creatureType : EnumCreatureType.values())
-        {
-            entryLoop:
-            for(Biome.SpawnListEntry entry : biome.getSpawnableList(creatureType))
-            {
-                ResourceLocation entityName = ForgeRegistries.ENTITIES.getKey(EntityRegistry.getEntry(entry.entityClass));
-                boolean containsEntry = false;
-
-                Iterator<Config> configIter = entityConfigs.iterator();
-
-                while(configIter.hasNext())
+                if(blockConfig.getBlock(entry.getKey()) != null)
                 {
-                    Config entityConfig = configIter.next();
-
-                    if(entityName != null && entityConfig.getString("entity").equals(entityName.toString()))
-                    {
-                        containsEntry = true;
-                    }
-
-                    entityObjects.add(entityConfig.serialize().getAsJsonObject());
-                    configIter.remove();
-
-                    if(containsEntry)
-                    {
-                        continue entryLoop;
-                    }
-                }
-
-                JsonObject entityObject = new JsonObject();
-                entityObject.addProperty("entity", ForgeRegistries.ENTITIES.getKey(EntityRegistry.getEntry(entry.entityClass)).toString());
-                entityObject.addProperty("creatureType", creatureType.toString().toLowerCase());
-                entityObject.addProperty("weight", entry.itemWeight);
-                entityObject.addProperty("minGroupCount", entry.minGroupCount);
-                entityObject.addProperty("maxGroupCount", entry.maxGroupCount);
-                entityObject.addProperty("spawn", true);
-                entityObjects.add(entityObject);
-            }
-        }
-
-        config.removeData("entities");
-        entityConfigs = config.getDataBranches("entities", entityObjects);
-
-        for(Config entityConfig : entityConfigs)
-        {
-            EntityEntry entry = ForgeRegistries.ENTITIES.getValue(entityConfig.getResource("entity"));
-
-            if(entry != null && entityConfig.getBoolean("spawn", true))
-            {
-                Class<? extends Entity> entityCls = entry.getEntityClass();
-                EnumCreatureType creatureType = entityConfig.getEnum("creatureType", EnumCreatureType.class);
-
-                if(EntityLiving.class.isAssignableFrom(entityCls))
-                {
-                    spawnableMobs.computeIfAbsent(creatureType, k -> new ArrayList<>()).add(new Biome.SpawnListEntry((Class<? extends EntityLiving>) entityCls, entityConfig.getInt("weight", 10), entityConfig.getInt("minGroupCount", 1), entityConfig.getInt("maxGroupCount", 4)));
+                    blocks.put(entry.getKey(), blockConfig.getBlock(entry.getKey()));
                 }
             }
-        }
 
-        List<Config> featureConfigs = config.getDataBranches("features", new ArrayList<>());
-        List<JsonObject> featureObjects = new ArrayList<>();
+            List<Config> entityConfigs = config.getDataBranches("entities", new ArrayList<>());
+            List<JsonObject> entityObjects = new ArrayList<>();
 
-        for(Config featureConfig : featureConfigs)
-        {
-            Feature feature = FeatureRegistry.createFeature(featureConfig.getResource("feature"), featureConfig);
-
-            if(feature != null && featureConfig.getBoolean("generate", true))
+            for(EnumCreatureType creatureType : EnumCreatureType.values())
             {
-                GenerationStage generationStage = featureConfig.getEnum("genStage", GenerationStage.class, GenerationStage.POST_DECORATE);
-                generationStageFeatures.computeIfAbsent(generationStage, k -> new ArrayList<>()).add(feature);
+                entryLoop:
+                for(Biome.SpawnListEntry entry : biome.getSpawnableList(creatureType))
+                {
+                    ResourceLocation entityName = ForgeRegistries.ENTITIES.getKey(EntityRegistry.getEntry(entry.entityClass));
+                    boolean containsEntry = false;
+
+                    Iterator<Config> configIter = entityConfigs.iterator();
+
+                    while(configIter.hasNext())
+                    {
+                        Config entityConfig = configIter.next();
+
+                        if(entityName != null && entityConfig.getString("entity").equals(entityName.toString()))
+                        {
+                            containsEntry = true;
+                        }
+
+                        entityObjects.add(entityConfig.serialize().getAsJsonObject());
+                        configIter.remove();
+
+                        if(containsEntry)
+                        {
+                            continue entryLoop;
+                        }
+                    }
+
+                    JsonObject entityObject = new JsonObject();
+                    entityObject.addProperty("entity", ForgeRegistries.ENTITIES.getKey(EntityRegistry.getEntry(entry.entityClass)).toString());
+                    entityObject.addProperty("creatureType", creatureType.toString().toLowerCase());
+                    entityObject.addProperty("weight", entry.itemWeight);
+                    entityObject.addProperty("minGroupCount", entry.minGroupCount);
+                    entityObject.addProperty("maxGroupCount", entry.maxGroupCount);
+                    entityObject.addProperty("spawn", true);
+                    entityObjects.add(entityObject);
+                }
             }
 
-            featureObjects.add(featureConfig.serialize().getAsJsonObject());
-        }
+            config.removeData("entities");
+            entityConfigs = config.getDataBranches("entities", entityObjects);
 
-        config.removeData("features");
-        config.getDataBranches("features", featureObjects);
-        enabled = config.getBoolean("enabled", true);
-        genDefaultFeatures = config.getBoolean("genDefaultFeatures", true);
+            for(Config entityConfig : entityConfigs)
+            {
+                EntityEntry entry = ForgeRegistries.ENTITIES.getValue(entityConfig.getResource("entity"));
+
+                if(entry != null && entityConfig.getBoolean("spawn", true))
+                {
+                    Class<? extends Entity> entityCls = entry.getEntityClass();
+                    EnumCreatureType creatureType = entityConfig.getEnum("creatureType", EnumCreatureType.class);
+
+                    if(EntityLiving.class.isAssignableFrom(entityCls))
+                    {
+                        spawnableMobs.computeIfAbsent(creatureType, k -> new ArrayList<>()).add(new Biome.SpawnListEntry((Class<? extends EntityLiving>) entityCls, entityConfig.getInt("weight", 10), entityConfig.getInt("minGroupCount", 1), entityConfig.getInt("maxGroupCount", 4)));
+                    }
+                }
+            }
+
+            List<Config> featureConfigs = config.getDataBranches("features", new ArrayList<>());
+            List<JsonObject> featureObjects = new ArrayList<>();
+
+            for(Config featureConfig : featureConfigs)
+            {
+                Feature feature = FeatureRegistry.createFeature(featureConfig.getResource("feature"), featureConfig);
+
+                if(feature != null && featureConfig.getBoolean("generate", true))
+                {
+                    GenerationStage generationStage = featureConfig.getEnum("genStage", GenerationStage.class, GenerationStage.POST_DECORATE);
+                    generationStageFeatures.computeIfAbsent(generationStage, k -> new ArrayList<>()).add(feature);
+                }
+
+                featureObjects.add(featureConfig.serialize().getAsJsonObject());
+            }
+
+            config.removeData("features");
+            config.getDataBranches("features", featureObjects);
+            enabled = config.getBoolean("enabled", true);
+            genDefaultFeatures = config.getBoolean("genDefaultFeatures", true);
+        }
     }
 
     public Biome getBiome()
