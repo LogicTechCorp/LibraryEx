8403fb19953d3fc7e9906ba07dc3c5fa1d322f88
LogicTechCorp <logictechcorp@gmail.com>
2018-07-27 00:45:08 -0700

    Added more block base classes

---
+++ src/main/java/lex/block/BlockBushLibEx.java
@@ -0,0 +1,137 @@
+/*
+ * LibEx
+ * Copyright (c) 2017-2018 by MineEx
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package lex.block;
+
+import lex.IModData;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
+
+public abstract class BlockBushLibEx extends BlockLibEx implements IPlantable
+{
+    protected static final AxisAlignedBB BUSH_AABB = new AxisAlignedBB(0.30000001192092896D, 0.0D, 0.30000001192092896D, 0.699999988079071D, 0.6000000238418579D, 0.699999988079071D);
+
+    public BlockBushLibEx(IModData data, String name, Material material)
+    {
+        super(data, name, material);
+        setSoundType(SoundType.PLANT);
+        setTickRandomly(true);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public abstract EnumPlantType getPlantType(IBlockAccess world, BlockPos pos);
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return BUSH_AABB;
+    }
+
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess world, BlockPos pos)
+    {
+        return NULL_AABB;
+    }
+
+    @Override
+    public void updateTick(World world, BlockPos pos, IBlockState state, Random rand)
+    {
+        checkAndDropBlock(world, pos, state);
+    }
+
+    @Override
+    public boolean canPlaceBlockAt(World world, BlockPos pos)
+    {
+        IBlockState soil = world.getBlockState(pos.down());
+        return super.canPlaceBlockAt(world, pos) && canSustainBush(soil);
+    }
+
+    @Override
+    public void neighborChanged(IBlockState state, World world, BlockPos pos, Block blockIn, BlockPos fromPos)
+    {
+        super.neighborChanged(state, world, pos, blockIn, fromPos);
+        checkAndDropBlock(world, pos, state);
+    }
+
+    @Override
+    public IBlockState getPlant(IBlockAccess world, BlockPos pos)
+    {
+        IBlockState state = world.getBlockState(pos);
+
+        if(state.getBlock() != this)
+        {
+            return getDefaultState();
+        }
+
+        return state;
+    }
+
+    protected abstract boolean canSustainBush(IBlockState state);
+
+    protected void checkAndDropBlock(World world, BlockPos pos, IBlockState state)
+    {
+        if(!canBlockStay(world, pos, state))
+        {
+            dropBlockAsItem(world, pos, state, 0);
+            world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+        }
+    }
+
+    public boolean canBlockStay(World world, BlockPos pos, IBlockState state)
+    {
+        if(state.getBlock() == this)
+        {
+            IBlockState soil = world.getBlockState(pos.down());
+            return canSustainBush(soil);
+        }
+
+        return canSustainBush(world.getBlockState(pos.down()));
+    }
+}
---
+++ src/main/java/lex/block/BlockRising.java
@@ -0,0 +1,139 @@
+/*
+ * LibEx
+ * Copyright (c) 2017-2018 by MineEx
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package lex.block;
+
+import lex.IModData;
+import lex.block.BlockLibEx;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
+
+public class BlockRising extends BlockLibEx
+{
+    public static boolean riseInstantly;
+
+    public BlockRising(IModData data, String name, Material material)
+    {
+        super(data, name, material);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand)
+    {
+        if(rand.nextInt(16) == 0)
+        {
+            BlockPos newPos = pos.up();
+
+            if(canRiseThrough(world.getBlockState(newPos)))
+            {
+                double posX = (double) ((float) pos.getX() + rand.nextFloat());
+                double posY = (double) pos.getY() - 0.05D;
+                double posZ = (double) ((float) pos.getZ() + rand.nextFloat());
+                world.spawnParticle(EnumParticleTypes.FALLING_DUST, posX, posY, posZ, 0.0D, 0.5D, 0.0D, Block.getStateId(state));
+            }
+        }
+    }
+
+    @Override
+    public int tickRate(World worldIn)
+    {
+        return 2;
+    }
+
+    @Override
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
+    {
+        worldIn.scheduleUpdate(pos, this, this.tickRate(worldIn));
+    }
+
+    @Override
+    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, net.minecraft.block.Block blockIn, BlockPos fromPos)
+    {
+        worldIn.scheduleUpdate(pos, this, this.tickRate(worldIn));
+    }
+
+    @Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        if(!worldIn.isRemote)
+        {
+            rise(worldIn, pos);
+        }
+    }
+
+    private void rise(World world, BlockPos pos)
+    {
+        if((world.isAirBlock(pos.down()) || canRiseThrough(world.getBlockState(pos.down()))) && pos.getY() >= 0)
+        {
+            if(!riseInstantly && world.isAreaLoaded(pos.add(-32, -32, -32), pos.add(32, 32, 32)))
+            {
+                if(!world.isRemote)
+                {
+                    //EntityFallingBlock entityfallingblock = new EntityFallingBlock(world, (double)pos.getX() + 0.5D, (double)pos.getY(), (double)pos.getZ() + 0.5D, world.getBlockState(pos));
+                    //onStartRising();
+                    //world.spawnEntity(entityfallingblock);
+                }
+            }
+            else
+            {
+                IBlockState state = world.getBlockState(pos);
+                world.setBlockToAir(pos);
+                BlockPos blockpos;
+
+                for(blockpos = pos.down(); (world.isAirBlock(blockpos) || canRiseThrough(world.getBlockState(blockpos))) && blockpos.getY() > 0; blockpos = blockpos.down())
+                {
+
+                }
+
+                if(blockpos.getY() > 0)
+                {
+                    world.setBlockState(blockpos.up(), state);
+                }
+            }
+        }
+    }
+
+    protected void onStartRising()
+    {
+
+    }
+
+    public static boolean canRiseThrough(IBlockState state)
+    {
+        Block block = state.getBlock();
+        Material material = state.getMaterial();
+        return block == Blocks.FIRE || material == Material.AIR || material == Material.WATER || material == Material.LAVA;
+    }
+
+    public void onFinishRising(World world, BlockPos pos, IBlockState p_176502_3_, IBlockState p_176502_4_)
+    {
+    }
+
+    public void onBroken(World world, BlockPos pos)
+    {
+    }
+}
