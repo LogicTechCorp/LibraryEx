681f5afdaf039b241eaad016f0408f2941ea3b6a
LogicTechCorp <logictechcorp@gmail.com>
2018-08-31 01:10:24 -0700

    Added a helper method to check if an entity is inside a block

--- src/main/java/lex/util/EntityHelper.java
+++ src/main/java/lex/util/EntityHelper.java
@@ -17,16 +17,21 @@
 
 package lex.util;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class EntityHelper
 {
-    private static Map<Class<? extends EntityLivingBase>, String> ENTITY_RESOURCE_LOCATION_CACHE = new HashMap<>();
+    private static final Map<Class<? extends EntityLivingBase>, String> ENTITY_RESOURCE_LOCATION_CACHE = new HashMap<>();
 
     public static String getEntityLocation(EntityLivingBase entity)
     {
@@ -36,4 +41,41 @@ public class EntityHelper
             return location != null ? location.toString() : null;
         });
     }
+
+    public static boolean isInBlock(Entity entity, IBlockState... states)
+    {
+        AxisAlignedBB boundingBox = entity.getEntityBoundingBox().grow(-0.10000000149011612D, -0.4000000059604645D, -0.10000000149011612D);
+        int minX = MathHelper.floor(boundingBox.minX);
+        int maxX = MathHelper.ceil(boundingBox.maxX);
+        int minY = MathHelper.floor(boundingBox.minY);
+        int maxY = MathHelper.ceil(boundingBox.maxY);
+        int minZ = MathHelper.floor(boundingBox.minZ);
+        int maxZ = MathHelper.ceil(boundingBox.maxZ);
+        BlockPos.PooledMutableBlockPos pooledMutableBlockPos = BlockPos.PooledMutableBlockPos.retain();
+
+        for(int posX = minX; posX < maxX; posX++)
+        {
+            for(int posY = minY; posY < maxY; posY++)
+            {
+                for(int posZ = minZ; posZ < maxZ; posZ++)
+                {
+                    IBlockState checkState = entity.getEntityWorld().getBlockState(pooledMutableBlockPos.setPos(posX, posY, posZ));
+
+                    for(IBlockState state : states)
+                    {
+                        if(state == checkState)
+                        {
+                            pooledMutableBlockPos.release();
+                            return true;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        pooledMutableBlockPos.release();
+        return false;
+    }
+
 }
