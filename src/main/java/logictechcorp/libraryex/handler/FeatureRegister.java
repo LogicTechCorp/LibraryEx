383d379dff4f53a7a19a16106427eb75fcafaeb2
LogicTechCorp <logictechcorp@gmail.com>
2019-09-02 18:00:17 -0700

    Created a custom ore feature

--- src/main/java/logictechcorp/libraryex/handler/FeatureRegister.java
+++ src/main/java/logictechcorp/libraryex/handler/FeatureRegister.java
@@ -19,6 +19,7 @@ package logictechcorp.libraryex.handler;
 
 import logictechcorp.libraryex.LibraryEx;
 import logictechcorp.libraryex.world.generation.feature.BiomeDataFeatureWrapper;
+import logictechcorp.libraryex.world.generation.feature.OreFeature;
 import logictechcorp.libraryex.world.generation.feature.PoolFeature;
 import logictechcorp.libraryex.world.generation.feature.TriplePlantFeature;
 import net.minecraft.world.gen.feature.Feature;
@@ -36,6 +37,7 @@ public class FeatureRegister
         registerFeature("biome_data_feature_wrapper", new BiomeDataFeatureWrapper(BiomeDataFeatureWrapper.Config::deserialize));
         registerFeature("triple_plant", new TriplePlantFeature(TriplePlantFeature.Config::deserialize));
         registerFeature("pool", new PoolFeature(PoolFeature.Config::deserialize));
+        registerFeature("ore", new OreFeature(OreFeature.Config::deserialize));
     }
 
     private static void registerFeature(String name, Feature<?> feature)
